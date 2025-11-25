21ff55e155f3eb9ee2ad79805daf749db3d71516
Waytid-way <mamaway083@gmail.com>
2025-11-26 00:42:25 +0700

    Remove old file

--- connect.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine
-
-engine = create_engine("postgresql://admin:admin@localhost:5432/hotel_dw")
-
-try:
-    conn = engine.connect()
-    print("Connected OK")
-except Exception as e:
-    print("Error:", e)
